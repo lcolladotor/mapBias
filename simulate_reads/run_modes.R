c608192184edca8fa984899ad55e00a659b183c7
lcolladotor <lcollado@jhsph.edu>
2015-02-20 11:13:04 -0500

    Trush rmarkdown with dir managment

--- simulate_reads/run_modes.R
+++ simulate_reads/run_modes.R
@@ -6,5 +6,4 @@ for(tophat in modes){
     dir.create(tophat, recursive = TRUE)
     startTime <- Sys.time()
     render('simulate_reads.Rmd', output_dir = file.path(mainDir, tophat))
-    setwd(mainDir)
 }
